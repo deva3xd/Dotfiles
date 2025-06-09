477c7dc368a16ef239ec8bea3dc3baa168b8af5d
deva3xd <devarn05@gmail.com>
2025-06-10 00:08:16 +0700

    update polybar file

--- config/polybar/get_network.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-color="%{F#FFFFFF}"
-network_name=$(nmcli -t -f ACTIVE,SSID dev wifi | grep '^yes' | cut -d':' -f2)
-if [ -z "$network_name" ]; then
-    echo "${color}󰖪"
-else
-    echo "${color}󰖩"
-fi
--- config/rofi/network.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-networks=$(nmcli -t -f SSID,SECURITY dev wifi | awk -F: '{print $1 " (" $2 ")"}' | rofi -dmenu -p "Networks:")
-selected_network=$(echo "$networks" | awk '{print $1}')
-
-if [ -n "$selected_network" ]; then
-    nmcli device wifi connect "$selected_network" || notify-send "Network Manager" "Failed to connect to $selected_network"
-else
-    notify-send "Network Manager" "No network selected."
-fi
